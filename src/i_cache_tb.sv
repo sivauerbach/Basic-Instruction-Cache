beb01b150064240ed55f05a8e239390db71c0c09
sivauerbach <114685221+sivauerbach@users.noreply.github.com>
2025-09-26 18:26:11 -0400

    creating top file, cleaning tb, renaming mem file

--- src/i_cache_tb.sv
+++ src/i_cache_tb.sv
@@ -12,55 +12,26 @@
 module i_cache_tb #(parameter DATA_WIDTH = 32,parameter ADD_WIDTH = 12)();
   reg clk;
 
-  wire [DATA_WIDTH-1:0] data_in;
-  wire [DATA_WIDTH-1:0] data_out;
-  wire [ADD_WIDTH-1:0]  rd_addr;
-  wire        rd_en;
-  wire [ADD_WIDTH-1:0]  wr_addr;
-  wire        wr_en;
-
   reg  [ADD_WIDTH-1:0] i_cache_addr = 0;
   reg  [DATA_WIDTH-1:0] i_cache_din = 0;
   reg         i_cache_rden = 0;
   reg         i_cache_wren = 0;
   wire        i_cache_hit_miss;
-  wire [DATA_WIDTH-1:0] i_cache_q;
+  wire [DATA_WIDTH-1:0] cpu_data_out;
 
-  i_cache DUT_CACHE (
-    .clock(clk),
-    .cpu_add(i_cache_addr),
-    .cpu_data_in(i_cache_din),
-    .cpu_ren(i_cache_rden),
-    .cpu_wen(i_cache_wren),
-    .hit_miss(i_cache_hit_miss),
-    .cpu_data_out(i_cache_q),
-    .m_data_out(data_in),
-    .m_rd_address(rd_addr),
-    .m_ren(rd_en),
-    .m_wr_address(wr_addr),
-    .m_wen(wr_en),
-    .m_data_in(data_out)
+  defparam DUT.ADD_WIDTH = ADD_WIDTH;
+  defparam DUT.DATA_WIDTH = DATA_WIDTH;
+  defparam DUT.MEM_FILE = "i_mem_data.txt";
+
+  i_cache_top DUT (
+    // inputs from testbench:    
+    .clk(clk),
+    .i_cache_addr(i_cache_addr),
+    .i_cache_din(i_cache_din),
+    // outputs  to testbench 
+    .i_cache_hit_miss(i_cache_hit_miss),
+    .cpu_data_out(cpu_data_out)
   );
-
-  // defparam DUT_CACHE.NWAYS = 4;
-  // defparam DUT_CACHE.NSETS = 128;
-  // defparam DUT_CACHE.BLOCK_SIZE = 32;
-  // defparam DUT_CACHE.WIDTH = 32;
-
-  mem DUT_MEM (
-    .clock(clk),
-    .data(data_in),
-    .rdaddress(rd_addr),
-    .rden(rd_en),
-    .wraddress(wr_addr),
-    .wden(wr_en),
-    .data_out(data_out)
-  );
-
-  defparam DUT_MEM.ADD_WIDTH = ADD_WIDTH;
-  defparam DUT_MEM.DATA_WIDTH = DATA_WIDTH;
-  defparam DUT_MEM.FILE = "i_mem_data.txt";
-
 
   always
     begin
@@ -211,8 +182,8 @@ module i_cache_tb #(parameter DATA_WIDTH = 32,parameter ADD_WIDTH = 12)();
 
   initial
   begin
-    //$monitor("time=%3d, addr=%1b, cpu_data_in=%1b, cpu_ren=%1b, cpu_wen=%1b, cpu_data_out=%1b, hit_miss=%1b", $time,i_cache_addr,i_cache_din,i_cache_rden,i_cache_wren,i_cache_q,i_cache_hit_miss);
-    $monitor("time=%4d | addr=%10d | hm=%b | cpu_data_out=%08x | way1=%08h | way2=%08h | mem1=%08h | lru1=%d | lru2=%d" ,$time,i_cache_addr,i_cache_hit_miss,DUT_CACHE.cpu_data_out,DUT_CACHE.mem1[1],DUT_CACHE.mem2[1],DUT_MEM.mem[1],DUT_CACHE.lru1[1],DUT_CACHE.lru2[1]);
+    $monitor("time=%3d, addr=%1h, cpu_data_in=%1h, cpu_ren=%1b, cpu_wen=%1b, cpu_data_out=%1h, hit_miss=%1b", $time,i_cache_addr,i_cache_din,i_cache_rden,i_cache_wren,cpu_data_out,i_cache_hit_miss);
+    // $monitor("time=%4d | addr=%10d | hm=%b | cpu_data_out=%08x | way1=%08h | way2=%08h | mem1=%08h | lru1=%d | lru2=%d" ,$time,i_cache_addr,i_cache_hit_miss,DUT_CACHE.cpu_data_out,DUT_CACHE.mem1[1],DUT_CACHE.mem2[1],DUT_MEM.mem[1],DUT_CACHE.lru1[1],DUT_CACHE.lru2[1]);
     
     // Simulate with Xcelium:
     $dumpfile("simulation_output.vcd");
---
+++ src/i_cache_top.sv
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+
+
+module i_cache_top #(parameter DATA_WIDTH = 32,parameter ADD_WIDTH = 12, parameter MEM_FILE = "")(
+
+    // inputs from testbench:
+    input wire  clk,
+    input wire  [ADD_WIDTH-1:0] i_cache_addr,
+    input wire  [DATA_WIDTH-1:0] i_cache_din,
+    input wire         i_cache_rden,
+    input wire         i_cache_wren,
+
+    // outputs  to testbench 
+    wire        i_cache_hit_miss,
+    wire [DATA_WIDTH-1:0] cpu_data_out
+);
+
+    // Internal wires between cache and memory
+    wire [DATA_WIDTH-1:0]   data_in;
+    wire [DATA_WIDTH-1:0]   data_out;
+    wire [ADD_WIDTH-1:0]    rd_addr;
+    wire                    rd_en;
+    wire [ADD_WIDTH-1:0]    wr_addr;
+    wire                    wr_en;
+
+
+    // defparam DUT_CACHE.NWAYS = 4;
+    // defparam DUT_CACHE.NSETS = 128;
+    // defparam DUT_CACHE.BLOCK_SIZE = 32;
+    // defparam DUT_CACHE.WIDTH = 32;
+
+    i_cache DUT_CACHE (
+        .clock(clk),
+        .cpu_add(i_cache_addr),
+        .cpu_data_in(i_cache_din),
+        .cpu_ren(i_cache_rden),
+        .cpu_wen(i_cache_wren),
+        .hit_miss(i_cache_hit_miss),
+        .cpu_data_out(cpu_data_out),
+        .m_data_out(data_in),
+        .m_rd_address(rd_addr),
+        .m_ren(rd_en),
+        .m_wr_address(wr_addr),
+        .m_wen(wr_en),
+        .m_data_in(data_out)
+    );
+
+    defparam DUT_MEM.ADD_WIDTH = ADD_WIDTH;
+    defparam DUT_MEM.DATA_WIDTH = DATA_WIDTH;
+    defparam DUT_MEM.MEM_FILE = "i_mem_data.txt";
+
+    mem DUT_MEM (
+        .clock(clk),
+        .data(data_in),
+        .rdaddress(rd_addr),
+        .rden(rd_en),
+        .wraddress(wr_addr),
+        .wden(wr_en),
+        .data_out(data_out)
+        );
+
+
+endmodule
